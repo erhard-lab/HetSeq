63449b5393a2a03ca2a807ceb02fcc06b93466cf
Kevin Berg <bergkevin@web.de>
2025-01-29 13:59:50 +0100

    CRAN release changes

---
+++ NEWS.md
@@ -0,0 +1,8 @@
+# HetSeq 0.1.0
+
+## Initial CRAN Release
+
+- First release of the `HetSeq` package.
+- Provides tools for creating cell-cell trajectories.
+- Implements methods for identifying pathway modulators by leveraging intercellular heterogeneity.
+- Includes plot functions to visualize the results.
--- R/hetseq.R
+++ R/hetseq.R
@@ -15,9 +15,9 @@ NULL
 #'
 #' - classify: Predicting cellular outcome by expression of single genes (+informative features) reveals genes with strong predictive capabilities (high AUC) as potential pathway modulators.
 #'
-#' - doubleML: A strict predictor that utilizes Causal Inference (doubleML) to distinguish causal factors from simply correlating genes. Genes with high estimated effects on the outcome and high significance are identified as potential pathway modulators.
+#' - DoubleML: A strict predictor that utilizes Causal Inference (DoubleML) to distinguish causal factors from simply correlating genes. Genes with high estimated effects on the outcome and high significance are identified as potential pathway modulators.
 #' 
-#' @param method The method to run Heterogeneity-seq. Calls hetseq.test, hetseq.classify or hetseq.doubleML.
+#' @param method The method to run Heterogeneity-seq. Calls hetseq.test, hetseq.classify or hetseq.DoubleML.
 #' @param ... Parameters given to the chosen Hetseq method. See respective help pages.
 #' @examples
 #' \dontrun{
@@ -28,15 +28,15 @@ NULL
 #' }
 #' @return Table of Heterogeneity-seq results.
 #' @export
-Hetseq = function(method=c("test", "classify", "doubleML"), ...){
+Hetseq = function(method=c("test", "classify", "DoubleML"), ...){
     if (method == "test") {
       return(HetseqTest(...))
     } else if (method == "classify") {
       return(HetseqClassify(...))
-    } else if (method == "doubleML") {
+    } else if (method == "DoubleML") {
       return(HetseqDoubleML(...))
     } else {
-      stop("Invalid hetseq method specified. Please use 'test', 'classify', or 'doubleML'.")
+      stop("Invalid hetseq method specified. Please use 'test', 'classify', or 'DoubleML'.")
     }
 }
 
@@ -98,8 +98,8 @@ HetseqClassify<-function(object, trajectories, score.group = NULL, score.name=NU
     if(is.null(score.name)){
       stop("Either provide a score.group or a score.name (Seurat metadata) on which to calculate the score.group")
     }
-    br1=quantile(object[[score.name]][trajectories[,length(colnames(trajectories))],], probs = quantiles)[1]
-    br2=quantile(object[[score.name]][trajectories[,length(colnames(trajectories))],], probs = quantiles)[2]
+    br1=stats::quantile(object[[score.name]][trajectories[,length(colnames(trajectories))],], probs = quantiles)[1]
+    br2=stats::quantile(object[[score.name]][trajectories[,length(colnames(trajectories))],], probs = quantiles)[2]
     score.group = cut(object[[score.name]][trajectories[,length(colnames(trajectories))],],breaks=c(-10,br1,br2,10),labels=c("Low","Middle","High"))
     object[["Trajectory"]] = "none"
     for(x in c(1:length(colnames(trajectories)))){
@@ -193,9 +193,9 @@ HetseqClassify<-function(object, trajectories, score.group = NULL, score.name=NU
   t
 }
 
-#' Heterogeneity-seq: Classifying cellular response by gene expression values including causal inference by doubleML
+#' Heterogeneity-seq: Classifying cellular response by gene expression values including causal inference by DoubleML
 #'
-#' Classifying the cellular response of control cells using single gene expression (+ informative features) to identify features with the strongest predictive capabilities and applying causal inference by a doubleML approach.
+#' Classifying the cellular response of control cells using single gene expression (+ informative features) to identify features with the strongest predictive capabilities and applying causal inference by a DoubleML approach.
 #' @param object Seurat object
 #' @param trajectories Matrix of cell-cell trajectories. Columns represent time points, rows represent trajectories of connected cells over time points.
 #' @param score.group A named vector of response groups. Names represent cells, the values represent the score groups. If no score.group is set, use score.name and quantiles parameters must be set to define score groups.
@@ -204,7 +204,7 @@ HetseqClassify<-function(object, trajectories, score.group = NULL, score.name=NU
 #' @param compareGroups Which score groups to test. Default: Low vs. High
 #' @param posClass Define the positive Class for classification.
 #' @param basefeatures Additional informative features to include in the classification. Must be meta data available in the Seurat object.
-#' @param background A set of genes that will be considered as potential confounding factors in the doubleML analysis. Must contain all genes set in the genes parameter. By default, all genes are used.
+#' @param background A set of genes that will be considered as potential confounding factors in the DoubleML analysis. Must contain all genes set in the genes parameter. By default, all genes are used.
 #' @param genes Vector of genes to test.
 #' @param assay The name of the Seurat assay to perform Heterogeneity-seq on. If NULL, the default assay will be used.
 #' @param split Set a training-test data split. Must be in [0,1]
@@ -234,8 +234,8 @@ HetseqDoubleML <- function(object, trajectories, score.group = NULL, score.name=
     if(is.null(score.name)){
       stop("Either provide a score.group or a score.name (Seurat metadata) on which to calculate the score.group")
     }
-    br1=quantile(object[[score.name]][trajectories[,length(colnames(trajectories))],], probs = quantiles)[1]
-    br2=quantile(object[[score.name]][trajectories[,length(colnames(trajectories))],], probs = quantiles)[2]
+    br1=stats::quantile(object[[score.name]][trajectories[,length(colnames(trajectories))],], probs = quantiles)[1]
+    br2=stats::quantile(object[[score.name]][trajectories[,length(colnames(trajectories))],], probs = quantiles)[2]
     score.group = cut(object[[score.name]][trajectories[,length(colnames(trajectories))],],breaks=c(-10,br1,br2,10),labels=c("Low","Middle","High"))
     object[["Trajectory"]] = "none"
     for(x in c(1:length(colnames(trajectories)))){
